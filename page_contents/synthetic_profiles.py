0f08e302ee687a58670d55ebd1f34fede8827569
j.aschauer <j.aschauer@oeko.de>
2025-02-12 11:05:48 +0100

    rework synthetic profiles page
    remove year,  code is not working for other years

--- page_contents/synthetic_profiles.py
+++ page_contents/synthetic_profiles.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """Content of Synthetic profiles tab."""
 
+import io
 from datetime import datetime
 
+import pandas as pd
 import plotly.express as px
 import streamlit as st
 
@@ -13,7 +15,32 @@ from src.load_generator import (
     modul_4_IND_E,
 )
 
-st.title("Synthetic load data")
+
+# download button for excel file:
+def download_excel_file(
+    filename: str,
+    df: pd.DataFrame,
+    label: str = "Tabelle als .xlsx herunterladen",
+):
+    buffer = create_excel_file(df)
+    st.download_button(
+        label=label,
+        icon=":material/download:",
+        data=buffer,
+        file_name=filename,
+        mime="application/vnd.ms-excel",
+    )
+
+
+@st.cache_data(show_spinner=False)
+def create_excel_file(df: pd.DataFrame) -> io.BytesIO:
+    buffer = io.BytesIO()
+    with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
+        df.to_excel(writer, merge_cells=False)
+    return buffer
+
+
+st.title("Generate synthetic load data")
 
 
 # """Lege Pfad fest, wo Ergebnisse abgelegt werden"""
@@ -21,15 +48,18 @@ PROFILES_DATA_PATH = "data/profiles"
 
 all_info_wz = modul_1_IND_E.get_industry_type_data(PROFILES_DATA_PATH)
 
-st.subheader("Eigenschaften aller Industrietypen:")
 
-st.dataframe(all_info_wz, column_config={"WZ_ID": st.column_config.TextColumn()})
+with st.expander("Characteristics of all industry types"):
+    st.dataframe(all_info_wz, column_config={"WZ_ID": st.column_config.TextColumn()})
 
 
-st.sidebar.subheader("Einstellungen:")
-year = st.sidebar.selectbox("Jahr:", [2018, 2019, 2020], index=1)
+st.header("Settings")
+settings_container = st.container(border=True)
+year = 2019  # we do not have data for 2017 and 2018, so we fix it to 2019
 
-industry_name = st.sidebar.selectbox("Industry type:", all_info_wz["Name"], index=0)
+with settings_container:
+    industry_name = st.selectbox("Industry type", all_info_wz["Name"], index=0)
+
 industry_number = int(
     all_info_wz.set_index("Name").at[industry_name, "industry_number"]
 )
@@ -46,26 +76,29 @@ industry_number = int(
     data_industry_type,
 ) = modul_1_IND_E.modul_1_el(industry_number, PROFILES_DATA_PATH)
 
-st.write(data_industry_type)
-st.sidebar.subheader("Parameter zu ausgewähltem Industrietyp:")
-data_industry_type["Fluktuation"] = st.sidebar.select_slider(
-    "Fluktuation:", range(0, 100), value=data_industry_type["Fluktuation"].values[0]
-)
-
-data_industry_type["Energieverbrauch 2019"] = st.sidebar.number_input(
-    "Energieverbrauch 2019:",
-    value=data_industry_type["Energieverbrauch 2019"].values[0],
-)
-
-data_industry_type["Peak_faktor"] = st.sidebar.number_input(
-    "Peak_faktor:",
-    value=data_industry_type["Peak_faktor"].values[0],
-)
-
-data_industry_type["Base_faktor"] = st.sidebar.number_input(
-    "Base_faktor:",
-    value=data_industry_type["Base_faktor"].values[0],
-)
+with settings_container:
+    cols = st.columns(4)
+    with cols[0]:
+        data_industry_type["Fluktuation"] = st.select_slider(
+            "Fluctuation",
+            range(0, 100),
+            value=data_industry_type["Fluktuation"].values[0],
+        )
+    with cols[1]:
+        data_industry_type["Energieverbrauch 2019"] = st.number_input(
+            "Energy consumption",
+            value=data_industry_type["Energieverbrauch 2019"].values[0],
+        )
+    with cols[2]:
+        data_industry_type["Peak_faktor"] = st.number_input(
+            "Peak factor",
+            value=data_industry_type["Peak_faktor"].values[0],
+        )
+    with cols[3]:
+        data_industry_type["Base_faktor"] = st.number_input(
+            "Base factor:",
+            value=data_industry_type["Base_faktor"].values[0],
+        )
 
 industry_type = data_industry_type["WZ_ID"][industry_number]
 
@@ -106,24 +139,29 @@ df_year_3 = modul_3_IND_E.normalising_1000(df)
 df_year_4 = modul_4_IND_E.modul_4(year, industry_number, df_year_3, data_industry_type)
 df_year_4 = modul_4_IND_E.modul_4_fluct(industry_number, df_year_4, data_industry_type)
 
-st.subheader("Final Profile")
+st.header("Final Profile")
+with st.container(border=True):
+    date_range = st.slider(
+        "select range for plot",
+        value=(datetime(2019, 1, 1, 0, 0), datetime(2019, 1, 14, 0, 0)),
+        min_value=datetime(2019, 1, 1, 0, 0),
+        max_value=datetime(2019, 12, 31, 23, 45),
+        format="YYYY/MM/DD - HH:mm",
+        label_visibility="hidden",
+    )
 
-date_range = st.slider(
-    "select range for plot",
-    value=(datetime(2019, 1, 1, 0, 0), datetime(2019, 1, 14, 0, 0)),
-    min_value=datetime(2019, 1, 1, 0, 0),
-    max_value=datetime(2019, 12, 31, 23, 45),
-    format="YYYY/MM/DD - HH:mm",
-    label_visibility="hidden",
-)
-
-fig = px.area(
-    df_year_4.drop("Total", axis=1)
-    .multiply(1e-3)
-    .loc[date_range[0] : date_range[1], :],
-    title=f"{industry_name} (WZ {industry_type})",
-    labels={"value": "MW", "index": "Zeit"},
-)
-st.plotly_chart(fig, use_container_width=True)
-with st.expander("Data"):
-    st.dataframe(df_year_4 * 1e-3, use_container_width=True)
+    fig = px.area(
+        df_year_4.drop("Total", axis=1)
+        .multiply(1e-3)
+        .loc[date_range[0] : date_range[1], :],
+        title=f"{industry_name} (WZ {industry_type})",
+        labels={"value": "MW", "index": "Zeit", "variable": "End use type"},
+    )
+    st.plotly_chart(fig, use_container_width=True)
+    with st.expander("Data"):
+        st.dataframe(df_year_4 * 1e-3, use_container_width=True)
+        download_excel_file(
+            f"synthetic-load-profiles-{industry_name.lower().replace(' ', '')}.xlsx",
+            df_year_4,
+            "Download table as .xlsx",
+        )
