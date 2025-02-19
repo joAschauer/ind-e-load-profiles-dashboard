bd07813c4a7f2035e69df53c660f2926ab1c5bb1
j.aschauer <j.aschauer@oeko.de>
2025-02-19 14:11:20 +0100

    add description of app

--- page_contents/home.py
+++ page_contents/home.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("IND-E industry load profiles")
+
+st.markdown(
+    "This dashboard lets you view and download synthetic load profiles for different industry sectors."
+)
