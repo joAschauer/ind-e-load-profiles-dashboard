718094a145983d591ec636b9ebb888900f391efe
j.aschauer <j.aschauer@oeko.de>
2025-04-08 09:56:24 +0200

    add funding statement logo

--- page_contents/home.py
+++ page_contents/home.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+
 import streamlit as st
 
 st.title("IND-E industry load profiles")
 
 st.markdown(
-    "This dashboard lets you view and download synthetic load profiles for different industry sectors."
+    """This dashboard lets you view and download synthetic load profiles for different industry sectors.
+    It was developed as part of the IND-E project.
+    """
 )
+st.image(Path("assets/BMWK_Fz_2017_Web2x_en.gif"))
